f904a90864dabb616d81fc89c06cdc044d297ebd
hope7a <107469692+hope7a@users.noreply.github.com>
2022-07-07 15:59:32 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,10 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+int i = 0;
 
-	for (i = 0; i < 10; i++)
-		if (i != 2 && i != 4)
-			_putchar(i + '0');
-	_putchar('\n');
+for (i = 0; i < 10; i++)
+if (i != 2 && i != 4)
+_putchar(i + '0');
+_putchar('\n');
 }
