5dedd880b41bb3a90c1ae5cec405a8ba1614b4bc
hope7a <107469692+hope7a@users.noreply.github.com>
2022-07-07 15:31:29 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - from 0 to 9 except 2 and 4.
+ * print_most_numbers - prints 0 to 9 except 2 and 4
  *
- * Return: 0-9 except 2 and 4
+ * Return: 0-9, except 2 and 4
  */
-
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ((i != 2) && (i != 4))
-			_putchar(i + 48);
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
