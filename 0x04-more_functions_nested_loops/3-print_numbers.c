8e31190313a13d88cc11163b9386f03210986dfe
hope7a <107469692+hope7a@users.noreply.github.com>
2022-07-07 15:26:36 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,20 @@
-#include "main.h"
-/**
-*print_numbers -  print numbers 0 to 9
-*Return: returns 0-9
-*/
+#include "holberton.h"
 
-void print_numbers(void)
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
+ */
+void print_most_numbers(void)
 {
-int number = 48;
-  
-for (number = 48; number < 58; number++)
-{
-_putchar(number);
-}
-_putchar(10);
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
 }
